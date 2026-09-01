ee9e8677516e0c55e8b21d46a8513168609692a6
takeokunn <bararararatty@gmail.com>
2026-09-02 07:02:49 +0900

    refactor: declare repl state table factories

--- src/presentation/repl-state.lisp
+++ src/presentation/repl-state.lisp
@@ -1,11 +1,11 @@
 (in-package #:nshell.presentation)
 
-;; REPL State
-(defun %make-repl-name-table ()
-  (make-hash-table :test #'equal))
+(defmacro %define-repl-state-table-factory (name test)
+  `(defun ,name ()
+     (make-hash-table :test ,test)))
 
-(defun %make-repl-process-registry ()
-  (make-hash-table :test #'eql))
+(%define-repl-state-table-factory %make-repl-name-table #'equal)
+(%define-repl-state-table-factory %make-repl-process-registry #'eql)
 
 (defun %make-repl-state-tables ()
   (values (%make-repl-name-table)

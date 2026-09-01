0a40a16f441719a716de369ce51c945f9423e7d6
takeokunn <bararararatty@gmail.com>
2026-09-01 18:18:26 +0900

    refactor: make redirect plans immutable

--- src/application/execute-pipeline-stage-external-data.lisp
+++ src/application/execute-pipeline-stage-external-data.lisp
@@ -4,11 +4,12 @@
             (:constructor %make-external-process-redirect-plan
                 (stdout-target stdout-mode stderr-target stderr-mode
                  stdout-endpoint stderr-endpoint merge-stderr-p))
+            (:copier nil)
             (:conc-name %external-process-redirect-plan-))
-  stdout-target
-  stdout-mode
-  stderr-target
-  stderr-mode
-  stdout-endpoint
-  stderr-endpoint
-  merge-stderr-p)
+  (stdout-target nil :read-only t)
+  (stdout-mode nil :read-only t)
+  (stderr-target nil :read-only t)
+  (stderr-mode nil :read-only t)
+  (stdout-endpoint nil :read-only t)
+  (stderr-endpoint nil :read-only t)
+  (merge-stderr-p nil :read-only t))

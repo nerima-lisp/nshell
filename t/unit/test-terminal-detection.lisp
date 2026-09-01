acf2c106474e8fad44b32bf4ed07fa1db06aef65
takeokunn <bararararatty@gmail.com>
2026-09-02 06:12:57 +0900

    test: cover terminal ioctl constants

--- t/unit/test-terminal-detection.lisp
+++ t/unit/test-terminal-detection.lisp
@@ -4,4 +4,10 @@
   (it "recognizes descriptors that are not terminals"
     "A non-interactive test process must never be classified as an interactive terminal."
     (expect (nshell.infrastructure.terminal:interactive-terminal-p 999999)
-            :to-be nil)))
+            :to-be nil))
+
+  (it "exposes platform terminal request constants"
+    (expect (plusp nshell.infrastructure.acl::+tiocswinsz+)
+            :to-be-truthy)
+    (expect (plusp nshell.infrastructure.acl::+tiocsctty+)
+            :to-be-truthy)))

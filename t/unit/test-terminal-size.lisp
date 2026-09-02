753b6a377da35cc4b99647f0b6bf5b9b362d700e
takeokunn <bararararatty@gmail.com>
2026-09-03 01:21:53 +0900

    test: cover negative terminal width fallback

--- t/unit/test-terminal-size.lisp
+++ t/unit/test-terminal-size.lisp
@@ -13,6 +13,12 @@
          (lambda () (values 24 0)))
       (expect 80 :to-equal (nshell.presentation::terminal-width))))
 
+  (it "uses the fallback width for a negative size"
+    (with-temporary-function
+        ('nshell.infrastructure.acl:get-terminal-size
+         (lambda () (values 24 -1)))
+      (expect 80 :to-equal (nshell.presentation::terminal-width))))
+
   (it "uses the fallback width when the size query fails"
     (with-temporary-function
         ('nshell.infrastructure.acl:get-terminal-size

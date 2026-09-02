720f909cedd85b2f28f4e80d742554a58643bf29
takeokunn <bararararatty@gmail.com>
2026-09-02 18:30:23 +0900

    refactor: model redirect plans as values

--- src/application/execute-pipeline-stage-external-data.lisp
+++ src/application/execute-pipeline-stage-external-data.lisp
@@ -1,18 +1,15 @@
 (in-package #:nshell.application)
 
-(defstruct (%external-process-redirect-plan
-            (:constructor %make-external-process-redirect-plan
-                (stdout-target stdout-mode stderr-target stderr-mode
-                 stdout-endpoint stderr-endpoint merge-stderr-p))
-            (:copier nil)
-            (:conc-name %external-process-redirect-plan-))
-  (stdout-target nil :read-only t)
-  (stdout-mode nil :read-only t)
-  (stderr-target nil :read-only t)
-  (stderr-mode nil :read-only t)
-  (stdout-endpoint nil :read-only t)
-  (stderr-endpoint nil :read-only t)
-  (merge-stderr-p nil :read-only t))
+(define-value-struct %external-process-redirect-plan
+    ((stdout-target nil)
+     (stdout-mode nil)
+     (stderr-target nil)
+     (stderr-mode nil)
+     (stdout-endpoint nil)
+     (stderr-endpoint nil)
+     (merge-stderr-p nil :type boolean))
+  :constructor %make-external-process-redirect-plan
+  :public-accessors nil)
 
 (defmacro %with-external-process-redirect-plan
     ((plan &key stdout-target stdout-mode stderr-target stderr-mode

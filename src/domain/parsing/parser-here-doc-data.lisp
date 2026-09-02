292baeb583a39f481f729f35075ed8134af52172
takeokunn <bararararatty@gmail.com>
2026-09-02 17:32:59 +0900

    refactor: model parser route values immutably

--- src/domain/parsing/parser-here-doc-data.lisp
+++ src/domain/parsing/parser-here-doc-data.lisp
@@ -1,39 +1,24 @@
 (in-package #:nshell.domain.parsing)
 
-(defstruct (%here-doc-delimiter-scan
-            (:constructor %make-here-doc-delimiter-scan
-                (reversed-delimiters))
-            (:copier nil))
-  (reversed-delimiters '() :type list :read-only t))
+(define-value-struct %here-doc-delimiter-scan
+  ((reversed-delimiters '() :type list)))
 
-(defstruct (%here-doc-line
-            (:constructor %make-here-doc-line
-                (text next-position newline-p))
-            (:copier nil))
-  (text "" :type string :read-only t)
-  (next-position nil :read-only t)
-  (newline-p nil :type boolean :read-only t))
+(define-value-struct %here-doc-line
+  ((text "" :type string)
+   (next-position nil)
+   (newline-p nil :type boolean)))
 
-(defstruct (%here-doc-body
-            (:constructor %make-here-doc-body
-                (body next-position missing-delimiter-p))
-            (:copier nil))
-  (body "" :type string :read-only t)
-  (next-position nil :read-only t)
-  (missing-delimiter-p nil :type boolean :read-only t))
+(define-value-struct %here-doc-body
+  ((body "" :type string)
+   (next-position nil)
+   (missing-delimiter-p nil :type boolean)))
 
-(defstruct (%here-doc-consumption
-            (:constructor %make-here-doc-consumption
-                (bodies next-position incomplete-p))
-            (:copier nil))
-  (bodies '() :type list :read-only t)
-  (next-position nil :read-only t)
-  (incomplete-p nil :type boolean :read-only t))
+(define-value-struct %here-doc-consumption
+  ((bodies '() :type list)
+   (next-position nil)
+   (incomplete-p nil :type boolean)))
 
-(defstruct (%here-doc-consumption-state
-            (:constructor %make-here-doc-consumption-state
-                (reversed-bodies next-position incomplete-p))
-            (:copier nil))
-  (reversed-bodies '() :type list :read-only t)
-  (next-position nil :read-only t)
-  (incomplete-p nil :type boolean :read-only t))
+(define-value-struct %here-doc-consumption-state
+  ((reversed-bodies '() :type list)
+   (next-position nil)
+   (incomplete-p nil :type boolean)))
--- src/domain/parsing/tokenizer-route-data.lisp
+++ src/domain/parsing/tokenizer-route-data.lisp
@@ -1,40 +1,33 @@
 (in-package #:nshell.domain.parsing)
 
-(defstruct (%fd-redirect-token-text
-            (:constructor %make-fd-redirect-token-text (value advance-count)))
-  (value "" :type string :read-only t)
-  (advance-count 0 :type fixnum :read-only t))
+(define-value-struct %fd-redirect-token-text
+  ((value "" :type string)
+   (advance-count 0 :type fixnum)))
 
 (defparameter +tokenizer-special-reader-dispatch-characters+
   '(#\( #\) #\# #\' #\")
   "Non-operator characters that route through tokenizer special handlers.")
 
-(defstruct (%tokenizer-special-dispatch-route
-            (:constructor %make-tokenizer-special-dispatch-route (character kind)))
-  (character nil :read-only t)
-  (kind nil :type (or null keyword) :read-only t))
+(define-value-struct %tokenizer-special-dispatch-route
+  ((character nil)
+   (kind nil :type (or null keyword))))
 
-(defstruct (%tokenizer-ampersand-route
-            (:constructor %make-tokenizer-ampersand-route (token-type value)))
-  (token-type :ampersand :type keyword :read-only t)
-  (value "&" :type string :read-only t))
+(define-value-struct %tokenizer-ampersand-route
+  ((token-type :ampersand :type keyword)
+   (value "&" :type string)))
 
-(defstruct (%tokenizer-pipe-route
-            (:constructor %make-tokenizer-pipe-route (token-type value)))
-  (token-type :pipe :type keyword :read-only t)
-  (value "|" :type string :read-only t))
+(define-value-struct %tokenizer-pipe-route
+  ((token-type :pipe :type keyword)
+   (value "|" :type string)))
 
-(defstruct (%tokenizer-right-angle-route
-            (:constructor %make-tokenizer-right-angle-route (kind value)))
-  (kind :redirect :type keyword :read-only t)
-  (value ">" :type (or null string) :read-only t))
+(define-value-struct %tokenizer-right-angle-route
+  ((kind :redirect :type keyword)
+   (value ">" :type (or null string))))
 
-(defstruct (%tokenizer-left-angle-route
-            (:constructor %make-tokenizer-left-angle-route (kind value)))
-  (kind :redirect :type keyword :read-only t)
-  (value "<" :type (or null string) :read-only t))
+(define-value-struct %tokenizer-left-angle-route
+  ((kind :redirect :type keyword)
+   (value "<" :type (or null string))))
 
-(defstruct (%tokenizer-left-paren-route
-            (:constructor %make-tokenizer-left-paren-route (kind end)))
-  (kind :literal :type keyword :read-only t)
-  (end nil :read-only t))
+(define-value-struct %tokenizer-left-paren-route
+  ((kind :literal :type keyword)
+   (end nil)))

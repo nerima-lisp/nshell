1ac6d1856f9f1992eb7f53d1062e98f0ae14ea53
takeokunn <bararararatty@gmail.com>
2026-09-02 16:27:16 +0900

    refactor: generate AST leaf node declarations with macro

--- src/domain/parsing/ast.lisp
+++ src/domain/parsing/ast.lisp
@@ -216,20 +216,22 @@ shorter argument and the command list is one longer than the separators."
     (%copy-ast-list body)
     span)
 
-(defstruct (argument-node (:include ast-node)
-                          (:constructor %make-argument-node (value &optional span)))
+(defmacro define-ast-leaf-node (name constructor lambda-list &body slots)
+  `(defstruct (,name (:include ast-node)
+                    (:constructor ,constructor ,lambda-list))
+     ,@slots))
+
+(define-ast-leaf-node argument-node %make-argument-node
+    (value &optional span)
   (value "" :type string :read-only t))
-
-(defstruct (operator-node (:include ast-node)
-                          (:constructor %make-operator-node (operator &optional span)))
+(define-ast-leaf-node operator-node %make-operator-node
+    (operator &optional span)
   (operator "" :type string :read-only t))
-
-(defstruct (error-node (:include ast-node)
-                       (:constructor %make-error-node (message position &optional span)))
+(define-ast-leaf-node error-node %make-error-node
+    (message position &optional span)
   (message "" :type string :read-only t)
   (position 0 :type integer :read-only t))
-
-(defstruct (incomplete-node (:include ast-node)
-                            (:constructor %make-incomplete-node (partial-text kind &optional span)))
+(define-ast-leaf-node incomplete-node %make-incomplete-node
+    (partial-text kind &optional span)
   (partial-text "" :type string :read-only t)
   (kind :unknown :type keyword :read-only t))

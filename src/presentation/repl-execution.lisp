4184ef12aab1cd7e4c72a7a8d3ca816ac391a440
takeokunn <bararararatty@gmail.com>
2026-09-04 18:55:04 +0900

    refactor: merge repl AST execution branches

--- src/presentation/repl-execution.lisp
+++ src/presentation/repl-execution.lisp
@@ -2,13 +2,10 @@
 
 (defun execute-ast (ast)
   (cond
-    ((nshell.domain.parsing:sequence-node-p ast)
-     (nth-value 1
-                (%with-repl-shell-context (context)
-                  (nshell.application:execute-ast-in-context context ast))))
     ((nshell.domain.parsing:command-node-p ast)
      (execute-command-node ast))
-    ((or (nshell.domain.parsing:pipeline-node-p ast)
+    ((or (nshell.domain.parsing:sequence-node-p ast)
+         (nshell.domain.parsing:pipeline-node-p ast)
          (nshell.domain.parsing:if-node-p ast)
          (nshell.domain.parsing:for-node-p ast)
          (nshell.domain.parsing:while-node-p ast)
